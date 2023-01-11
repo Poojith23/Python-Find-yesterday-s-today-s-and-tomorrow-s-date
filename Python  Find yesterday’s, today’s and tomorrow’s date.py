07d2b3fd4e07b77381fbcb46683528cad81cc892
Poojith23 <114901320+Poojith23@users.noreply.github.com>
2023-01-11 22:16:19 +0530

    Add files via upload

---
+++ Python  Find yesterday’s, today’s and tomorrow’s date.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Import datetime and timedelta
+# class from datetime module
+from datetime import datetime, timedelta
+ 
+ 
+# Get today's date
+presentday = datetime.now() # or presentday = datetime.today()
+ 
+# Get Yesterday
+yesterday = presentday - timedelta(1)
+ 
+# Get Tomorrow
+tomorrow = presentday + timedelta(1)
+ 
+ 
+# strftime() is to format date according to
+# the need by converting them to string
+print("Yesterday = ", yesterday.strftime('%d-%m-%Y'))
+print("Today = ", presentday.strftime('%d-%m-%Y'))
+print("Tomorrow = ", tomorrow.strftime('%d-%m-%Y'))
+
